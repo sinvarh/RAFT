5d6c45dd88fdb02120e03b0bf36abdf54732bc7a
sinvar <779272536@qq.com>
2020-10-16 00:21:49 +0800

    init

---
+++ raft.py
@@ -0,0 +1,14 @@
+class raft:
+    def __init__(self):
+        self.current_term
+        self.voted_for
+        self.log
+
+        self.commit_index
+        self.last_applied
+
+        self.next_index
+        self.match_index
+
+    def append_rpc(self,term,leader_id ,prev_log_index,prev_log_term,entries,leader_commit):
+
