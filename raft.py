011e04fccfac176dfc2842eff90b23b15dd23c5b
sinvar <779272536@qq.com>
2020-10-28 01:42:04 +0800

    完成raft日志追加前三点，状态尚未维护

--- raft.py
+++ raft.py
@@ -5,7 +5,7 @@ class raft:
     def __init__(self):
         self.current_term = 0
         self.voted_for =None
-        self.log = collections.OrderedDict()
+        self.log = []
 
         self.commit_index =0
         self.last_applied = 0
@@ -14,17 +14,33 @@ class raft:
         self.match_index =  ()
 
 
-    def append_rpc(self,term,leader_id ,prev_log_index,prev_log_term,entries,leader_commit):
-        if term<self.current_term:
-            return False
-        if prev_log_index not in self.log and self.log[prev_log_index][0] is not prev_log_term :
-            return False
-        if()
 
-
-        return
     def append_entries_rpc(self,term,leader_id ,prev_log_index,prev_log_term,entries,leader_commit):
-        return
+        #参考论文5.1，5.3
+        if(term<self.current_term):
+            return False
+        if(len(self.log)!= 0 and self.log[prev_log_index][0] != prev_log_term):
+            return False
+        if (len(entries) != 0):
+            for entry in entries:
+                #新条目索引
+                index = entry[0]
+                #新条目任期
+                index_term = entry[1]
+                if(self.log[index][0]!=index_term):
+                    #把这条数据往后得log都删除
+                    del self.log[index:]
+                    break
+            #正常追加
+            for entry in entries:
+                # 新条目索引
+                index = entry[0]
+                # 新条目任期
+                index_term = entry[1]
+                if(len(self.log)<index+1):
+                    self.log.append(entry)
+
+
 
     def request_vote_rpc(self,term, candidate_id,last_log_index,last_log_term):
         return
